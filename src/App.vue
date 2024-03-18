bbb2d68bba138b6b63d722439221aa73bd6afe05
Favor96 <Abbeyfavor@gmail.com>
2024-03-18 22:48:48 +0000

    Configuration et mise en place du projet

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div id="app">
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-red-500">
+    <h1>Ouiiiiff</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+</style>
