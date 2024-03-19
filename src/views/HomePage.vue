58ae14fbb872a6ba739e11b30ea7233cf4049e90
Favor96 <Abbeyfavor@gmail.com>
2024-03-19 00:57:27 +0000

    Configuration de la palette de couleur, des composat ,pages  et de la typographie

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="bg-red-500">
-    <h1>Ouiiiiff</h1>
+  <div class="bg-brandGradiant m-14 mt-0 h-28">
+    <div>
+      <h1 class=" text-deskH1 text-brandGradiant ">heading</h1>
+      <h1 class="text-deskH3 text-black">Ouiiiiff heg</h1>
+      <h1>Ouiiiiff</h1>
+      <h1>Ouiiiiff</h1>
+      <h1>Ouiiiiff</h1>
+
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,102 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'brandGradiant': 'linear-gradient(to right, #FE572E, #FC3A79)',
+      },
+      fontFamily: {
+        'sans': ['Alef', 'sans-serif'],
+      },
+      fontSize: {
+        'deskH1': '191px', 
+        'deskH2': '84px', 
+        'deskH3': '59px', 
+        'deskH4': '42px', 
+        'deskH5': '30px', 
+        'deskH6': '21px', 
+
+        'phoneH1': '55px', 
+        'phoneH2': '44px', 
+        'phoneH3': '35px', 
+        'phoneH4': '28px', 
+        'phoneH5': '23px', 
+        'phoneH6': '18px', 
+
+        'tabH1': '102px', 
+        'tabH2': '72px', 
+        'tabH3': '51px', 
+        'tabH4': '36px', 
+        'tabH5': '25px', 
+        'tabH6': '18px', 
+      },
+      letterSpacing: {
+        'deskH1': '0%', 
+        'deskH2': '0%', 
+        'deskH3': '0%', 
+        'deskH4': '0%', 
+        'deskH5': '0%', 
+        'deskH6': '0%',  
+      },
+      lineHeight: {
+        'deskH1': 'auto', 
+        'deskH2': 'auto', 
+        'deskH3': 'auto', 
+        'deskH4': 'auto', 
+        'deskH5': 'auto', 
+        'deskH6': 'auto', 
+
+        'phoneH1': 'auto', 
+        'phoneH2': '56px', 
+        'phoneH3': '48px', 
+        'phoneH4': '40px', 
+        'phoneH5': '28px', 
+        'phoneH6': '24px', 
+
+        'tabH1': '132px', 
+        'tabH2': '100px', 
+        'tabH3': '72px', 
+        'tabH4': '48px', 
+        'tabH5': '32px', 
+        'tabH6': '24px', 
+      },
+      fontWeight: {
+        'deskH1': 'bold', 
+        'deskH2': 'bold', 
+        'deskH3': 'bold', 
+
+        'phoneH1': 'bold', 
+        'phoneH2': 'bold', 
+        'phoneH3': 'bold', 
+
+        'tabH1': 'bold', 
+        'tabH2': 'bold', 
+        'tabH3': 'bold', 
+      },
+      colors:{
+        primary: {
+          DEFAULT: '#FC3A79',
+        },
+        secondary: {
+          DEFAULT: '#FE572E',
+        },
+        eroor: {
+          DEFAULT: '#E63329'
+        },
+        brandGradiant:{
+          DEFAULT: 'linear-gradient(to right, #FE572E, #FC3A79)'
+        },
+        black: {
+          DEFAULT: '#261F22',
+          50: 'rgba(38, 31, 34, 0.5)',   // Opacité 50%
+          75: 'rgba(38, 31, 34, 0.75)',  // Opacité 75%
+          25: 'rgba(38, 31, 34, 0.25)',  // Opacité 25%
+          10: 'rgba(38, 31, 34, 0.1)',   // Opacité 10%
+          5: 'rgba(38, 31, 34, 0.05)',  
+        },
+
+      }
+    },
   },
   plugins: [
     typography,
